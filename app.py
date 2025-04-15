80ded083aaa61d83fc6ad966c90cefcfb9c98ea2
choudharydivik <divik1820@outlook.com>
2025-04-16 00:16:24 +0530

    WebApp Integration and Deployment

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 
 from sklearn.preprocessing import StandardScaler
+from src.pipeline.predict_pipeline import CustomData,PredictPipeline
 
 application=Flask(__name__)
 
@@ -17,4 +18,33 @@ def predict_employment():
     if request.method=='GET':
         return render_template('home.html')
     else:
-        data=CustomData
+        data = CustomData(
+            CGPA=float(request.form.get('CGPA')),
+            Internships=int(request.form.get('Internships')),
+            Projects=int(request.form.get('Projects')),
+            Certifications=int(request.form.get('Certifications')),
+            Aptitude_Test_Score=float(request.form.get('Aptitude_Test_Score')),
+            Soft_Skills_Rating=float(request.form.get('Soft_Skills_Rating')),
+            Extracurricular_Activities=request.form.get('Extracurricular_Activities'),
+            Placement_Training=request.form.get('Placement_Training'),
+            SSC_Marks=float(request.form.get('SSC_Marks')),
+            HSC_Marks=float(request.form.get('HSC_Marks'))
+        )
+
+        pred_df = data.get_data_as_data_frame()
+        print(pred_df)
+        print("Before Prediction")
+        print(pred_df)
+        predict_pipeline = PredictPipeline()
+        print("Mid Prediction")
+        results = predict_pipeline.predict(pred_df)
+        print("After Prediction")
+
+        return render_template('home.html', results=results[0])
+    
+if __name__=="__main__":        
+    app.run(host="0.0.0.0",debug=True) 
+
+
+
+
--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -1,12 +1,81 @@
-import sys 
+import sys
+import os
 import pandas as pd
 from src.exception import CustomException
 from src.utils import load_object
+
 
 class PredictPipeline:
     def __init__(self):
         pass
 
+    def predict(self, features):
+        try:
+            model_path = os.path.join("artifacts", "model.pkl")
+            preprocessor_path = os.path.join("artifacts", "preprocessor.pkl")
+            labelencoder_path = os.path.join("artifacts", "label_encoder.pkl")
 
-# class CustomData:
-#     def __init__(self,CGPA:int,Internships:int,Projects:int,Certifications:int,Aptitude_Test_Score:int,Soft_Skills_Rating:int):
+            print("Before Loading")
+            model = load_object(file_path=model_path)
+            preprocessor = load_object(file_path=preprocessor_path)
+            label_encoder = load_object(file_path=labelencoder_path)
+            print("After Loading")
+
+
+            print(features)
+
+            data_scaled = preprocessor.transform(features)
+            preds_encoded = model.predict(data_scaled)
+            preds_encoded = preds_encoded.astype(int).ravel()
+            preds = label_encoder.inverse_transform(preds_encoded)
+            return preds
+
+        except Exception as e:
+            raise CustomException(e, sys)
+
+
+
+class CustomData:
+    def __init__(
+        self,
+        CGPA: float,
+        Internships: int,
+        Projects: int,
+        Certifications: int,
+        Aptitude_Test_Score: float,
+        Soft_Skills_Rating: float,
+        Extracurricular_Activities: str,
+        Placement_Training: str,
+        SSC_Marks: float,
+        HSC_Marks: float
+    ):
+        self.CGPA = CGPA
+        self.Internships = Internships
+        self.Projects = Projects
+        self.Certifications = Certifications
+        self.Aptitude_Test_Score = Aptitude_Test_Score
+        self.Soft_Skills_Rating = Soft_Skills_Rating
+        self.Extracurricular_Activities = Extracurricular_Activities
+        self.Placement_Training = Placement_Training
+        self.SSC_Marks = SSC_Marks
+        self.HSC_Marks = HSC_Marks
+
+    def get_data_as_data_frame(self):
+        try:
+            custom_data_input_dict = {
+                "CGPA": [self.CGPA],
+                "Internships": [self.Internships],
+                "Projects": [self.Projects],
+                "Certifications": [self.Certifications],
+                "Aptitude_Test_Score": [self.Aptitude_Test_Score],
+                "Soft_Skills_Rating": [self.Soft_Skills_Rating],
+                "Extracurricular_Activities": [self.Extracurricular_Activities],
+                "Placement_Training": [self.Placement_Training],
+                "SSC_Marks": [self.SSC_Marks],
+                "HSC_Marks": [self.HSC_Marks],
+            }
+
+            return pd.DataFrame(custom_data_input_dict)
+
+        except Exception as e:
+            raise CustomException(e, sys)
